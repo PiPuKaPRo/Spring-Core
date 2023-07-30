8e624bf6361c2173a800b1e7e91f2984f7de6dc5
Evgeniy Sakov <91192469+PiPuKaPRo@users.noreply.github.com>
2023-07-30 20:48:29 +0300

    Add files via upload

--- spapp1/src/main/java/ru/evg/springapp/TestSpring.java
+++ spapp1/src/main/java/ru/evg/springapp/TestSpring.java
@@ -8,16 +8,17 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        Music m1 = context.getBean("someRockMusic", Music.class);
-        Music m2 = context.getBean("someClassicalMusic", Music.class);
+        MusicPlayer mp = context.getBean("musicPlayer", MusicPlayer.class);
+//        Music m1 = context.getBean("someRockMusic", Music.class);
+//        Music m2 = context.getBean("someClassicalMusic", Music.class);
+//
+//        MusicPlayer mp1 = new MusicPlayer(m1);
+//        MusicPlayer mp2 = new MusicPlayer(m2);
+//
+//        mp1.playMusic();
+//        mp2.playMusic();
 
-        MusicPlayer mp1 = new MusicPlayer(m1);
-        MusicPlayer mp2 = new MusicPlayer(m2);
-
-        mp1.playMusic();
-        mp2.playMusic();
-
-//        mp.playPlayList();
+            mp.playPlayList();
 //
 //        System.out.println(mp.getName());
 //        System.out.println(mp.getVolume());
