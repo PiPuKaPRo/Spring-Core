0feba68d400400b8dee2368b7430cab3c519b094
Evgeniy Sakov <91192469+PiPuKaPRo@users.noreply.github.com>
2023-09-26 15:34:56 +0300

    Add files via upload

---
+++ spmvcapp/src/main/java/ru/sakov/springmvccourse/dao/PersonDao.java
@@ -0,0 +1,46 @@
+package ru.sakov.springmvccourse.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import ru.sakov.springmvccourse.models.Person;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Person> index() {
+        return jdbcTemplate.query("SELECT * FROM Person", new BeanPropertyRowMapper<>(Person.class));
+    }
+
+    public Person show(int id) {
+        return jdbcTemplate.query("SELECT * FROM Person WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>
+                        (Person.class))
+                .stream().findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+        jdbcTemplate.update("INSERT INTO Person VALUES(1, ?, ?, ?)", person.getName(),  person.getAge(),
+                person.getEmail());
+    }
+
+    public void update(int id, Person updPerson) {
+        jdbcTemplate.update("UPDATE Person SET name=?, age=?, email=? WHERE id=?", updPerson.getName(),
+                updPerson.getAge(), updPerson.getEmail(), id);
+    }
+
+    public void delete(int id) {
+        jdbcTemplate.update("DELETE FROM Person WHERE id=?", id);
+    }
+}
